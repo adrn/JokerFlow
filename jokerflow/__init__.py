6debf6d81d3af2ac349402e066a9b443c65f04a7
Dan F-M <danfm@nyu.edu>
2018-04-09 13:59:51 -0400

    adding multibody fit

--- jokerflow/__init__.py
+++ jokerflow/__init__.py
@@ -6,17 +6,40 @@ __all__ = ["kepler"]
 
 import os
 import sysconfig
-
 import tensorflow as tf
 
+# Load the ops library
 suffix = sysconfig.get_config_var("EXT_SUFFIX")
 dirname = os.path.dirname(os.path.abspath(__file__))
-mod = tf.load_op_library(os.path.join(dirname, "kepler" + suffix))
+libfile = os.path.join(dirname, "kepler_op")
+if suffix is not None:
+    libfile += suffix
+else:
+    libfile += ".so"
+kepler_op = tf.load_op_library(libfile)
 
-kepler = mod.kepler
+
+def kepler(M, e, **kwargs):
+    """Solve Kepler's equation
+
+    Args:
+        M: A Tensor of mean anomaly values.
+        e: A Tensor of eccentricities.
+        maxiter (Optional): The maximum number of iterations to run.
+        tol (Optional): The convergence tolerance.
+
+    Returns:
+        A Tensor with the eccentric anomaly evaluated for each entry in ``M``.
+
+    """
+    return kepler_op.kepler(M, e, **kwargs)
 
 
 @tf.RegisterGradient("Kepler")
 def _kepler_grad(op, *grads):
-    args = list(op.inputs) + list(op.outputs) + list(grads)
-    return mod.kepler_grad(*args)
+    M, e = op.inputs
+    E = op.outputs[0]
+    bE = grads[0]
+    bM = bE / (1.0 - e * tf.cos(E))
+    be = tf.sin(E) * bM
+    return [bM, be]
